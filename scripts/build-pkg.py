1a25525347a5d358a970452e28e77d4745ed0da6
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2022-12-08 16:05:49 +0800

    update build script.

--- scripts/build-pkg.py
+++ scripts/build-pkg.py
@@ -23,15 +23,23 @@ docker_images = {
     'centos6.5:test':'ossfs-centos6.5:test',
     'centos7.0:dev':'ossfs-centos7.0:dev',
     'centos7.0:test':'ossfs-centos7.0:test',
+    'centos8.0:dev':'ossfs-centos8.0:dev',
+    'centos8.0:test':'ossfs-centos8.0:test',
     'ubuntu14.04:dev':'ossfs-ubuntu14.04:dev',
     'ubuntu14.04:test':'ossfs-ubuntu14.04:test',
     'ubuntu16.04:dev':'ossfs-ubuntu16.04:dev',
     'ubuntu16.04:test':'ossfs-ubuntu16.04:test',
     'ubuntu18.04:dev':'ossfs-ubuntu18.04:dev',
     'ubuntu18.04:test':'ossfs-ubuntu18.04:test',
+    'ubuntu20.04:dev':'ossfs-ubuntu20.04:dev',
+    'ubuntu20.04:test':'ossfs-ubuntu20.04:test',
+    'anolisos7.0:dev':'ossfs-anolisos7.0:dev',
+    'anolisos7.0:test':'ossfs-anolisos7.0:test',
+    'anolisos8.0:dev':'ossfs-anolisos8.0:dev',
+    'anolisos8.0:test':'ossfs-anolisos8.0:test',
 }
 
-os_list = ['centos6.5', 'centos7.0', 'ubuntu14.04', 'ubuntu16.04', 'ubuntu18.04']
+os_list = ['centos7.0', 'centos8.0', 'ubuntu14.04', 'ubuntu16.04', 'ubuntu18.04', 'ubuntu20.04', 'anolisos7.0', 'anolisos8.0']
 working_dir = '/tmp/ossfs'
 dest_dir = '/var/ossfs'
 ossfs_source_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -46,14 +54,14 @@ def get_ossfs_version():
 ossfs_version = get_ossfs_version()
 
 def random_string(length):
-    return ''.join(random.choice(string.lowercase) for i in range(length))
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
 def exec_cmd(cmd):
-    print cmd
+    print(cmd)
     p = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
     out,err = p.communicate()
     if p.returncode != 0:
-        print "failed to run: " + cmd
+        print("failed to run: " + cmd)
         raise RuntimeError("Failed to run: %s\n%s" % (cmd,err))
 
 def docker_image_exist(name):
@@ -81,11 +89,11 @@ def docker_run(conatiner_name, img, volume_list, cmd):
     else:
         cmd = 'docker run --rm=true --name %s %s %s' % (conatiner_name, img, cmd)
 
-    print cmd
+    print(cmd)
     p = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
     out,err = p.communicate()
-    print "=====DOCKER INFO====="
-    print out
+    print("=====DOCKER INFO=====")
+    print(out)
     exitcode = p.returncode
 
     if exitcode != 0:
@@ -183,6 +191,34 @@ def command_test_package_centos70():
     command_test_package(f)
     f.close()
 
+def command_build_package_centos(os_name):
+    """
+    Generate the build package script running in docker container
+    """
+    cmd_dir = os.path.join(working_dir, 'command')
+    install_dir = '/tmp/ossfs_install'
+    f = open(os.path.join(cmd_dir, 'build_package_%s.sh'%os_name), 'w')
+    f.write('#!/bin/bash\n')
+    command_build_package(f, install_dir)
+    f.write('fpm -s dir -t rpm -n ossfs -v %s -C %s -p ossfs_VERSION_%s_ARCH.rpm -d "fuse >= 2.8.4" -d "fuse-libs >= 2.8.4" -d "libcurl >= 7.0" -d "libxml2 >= 2.6" -d "openssl-libs >= 0.9"\n' % (ossfs_version, install_dir, os_name))
+    f.close()
+
+def command_test_package_centos(os_name):
+    """
+    Generate the test package script running in docker container
+    """
+    pkg_list = glob.glob(os.path.join(working_dir, 'package/*%s*'%os_name))
+    if not pkg_list:
+        raise RuntimeError("Can not found %s package! May be build fail?"%os_name)
+    pkg = ntpath.basename(pkg_list[0])
+    cmd_dir = os.path.join(working_dir, 'command')
+    test_dir = os.path.join(dest_dir, 'source/test')
+    f = open(os.path.join(cmd_dir, 'test_package_%s.sh'%os_name), 'w')
+    f.write('#!/bin/bash\n')
+    f.write('yum -y localinstall %s/package/%s --nogpgcheck\n' % (dest_dir, pkg))
+    command_test_package(f)
+    f.close()
+
 def command_build_package_ubuntu(os_name):
     """
     Generate the build package script running in docker container
@@ -212,6 +248,34 @@ def command_test_package_ubuntu(os_name):
     command_test_package(f)
     f.close()
 
+def command_build_package_anolisos(os_name):
+    """
+    Generate the build package script running in docker container
+    """
+    cmd_dir = os.path.join(working_dir, 'command')
+    install_dir = '/tmp/ossfs_install'
+    f = open(os.path.join(cmd_dir, 'build_package_%s.sh'%os_name), 'w')
+    f.write('#!/bin/bash\n')
+    command_build_package(f, install_dir)
+    f.write('fpm -s dir -t rpm -n ossfs -v %s -C %s -p ossfs_VERSION_%s_ARCH.rpm -d "fuse >= 2.8.4" -d "fuse-libs >= 2.8.4" -d "libcurl >= 7.0" -d "libxml2 >= 2.6" -d "openssl-libs >= 0.9"\n' % (ossfs_version, install_dir, os_name))
+    f.close()
+
+def command_test_package_anolisos(os_name):
+    """
+    Generate the test package script running in docker container
+    """
+    pkg_list = glob.glob(os.path.join(working_dir, 'package/*%s*'%os_name))
+    if not pkg_list:
+        raise RuntimeError("Can not found %s package! May be build fail?"%os_name)
+    pkg = ntpath.basename(pkg_list[0])
+    cmd_dir = os.path.join(working_dir, 'command')
+    test_dir = os.path.join(dest_dir, 'source/test')
+    f = open(os.path.join(cmd_dir, 'test_package_%s.sh'%os_name), 'w')
+    f.write('#!/bin/bash\n')
+    f.write('yum -y localinstall %s/package/%s --nogpgcheck\n' % (dest_dir, pkg))
+    command_test_package(f)
+    f.close()
+
 def build_package():
     prepare()
     for os_name in os_list:
@@ -221,50 +285,82 @@ def build_package():
 
         if os_name == 'centos6.5':
             # build package
-            print "==========================="
-            print "build centos6.5 package ..."
-            print "==========================="
+            print("===========================")
+            print("build centos6.5 package ...")
+            print("===========================")
             command_build_package_centos65()
             container_name = 'ossfs_%s'%random_string(5)
             docker_run(container_name, dev_image, volumes, '/bin/bash %s/command/build_package_centos65.sh'%dest_dir)
 
             # test package
-            print "=========================="
-            print "test centos6.5 package ..."
-            print "=========================="
+            print("==========================")
+            print("test centos6.5 package ...")
+            print("==========================")
             command_test_package_centos65()
             container_name = 'ossfs_%s'%random_string(5)
             docker_run(container_name, test_image, volumes, '/bin/bash %s/command/test_package_centos65.sh'%dest_dir)
         elif os_name == 'centos7.0':
             # build package
-            print "==========================="
-            print "build centos7.0 package ..."
-            print "==========================="
+            print("===========================")
+            print("build centos7.0 package ...")
+            print("===========================")
             command_build_package_centos70()
             container_name = 'ossfs_%s'%random_string(5)
             docker_run(container_name, dev_image, volumes, '/bin/bash %s/command/build_package_centos70.sh'%dest_dir)
 
             # test package
-            print "=========================="
-            print "test centos7.0 package ..."
-            print "=========================="
+            print("==========================")
+            print("test centos7.0 package ...")
+            print("==========================")
             command_test_package_centos70()
             container_name = 'ossfs_%s'%random_string(5)
             docker_run(container_name, test_image, volumes, '/bin/bash %s/command/test_package_centos70.sh'%dest_dir)
+        elif os_name.startswith('centos'):
+            # build package
+            print("=============================")
+            print("build %s package ..." % os_name)
+            print("=============================")
+            command_build_package_centos(os_name)
+            container_name = 'ossfs_%s'%random_string(5)
+            docker_run(container_name, dev_image, volumes, '/bin/bash %s/command/build_package_%s.sh' % (dest_dir, os_name))
+
+            # test package
+            print("============================")
+            print("test %s package ..." % os_name)
+            print("============================")
+            command_test_package_centos(os_name)
+            container_name = 'ossfs_%s'%random_string(5)
+            docker_run(container_name, test_image, volumes, '/bin/bash %s/command/test_package_%s.sh' % (dest_dir, os_name))
         elif os_name.startswith('ubuntu'):
             # build package
-            print "============================="
-            print "build %s package ..." % os_name
-            print "============================="
+            print("=============================")
+            print("build %s package ..." % os_name)
+            print("=============================")
             command_build_package_ubuntu(os_name)
             container_name = 'ossfs_%s'%random_string(5)
             docker_run(container_name, dev_image, volumes, '/bin/bash %s/command/build_package_%s.sh' % (dest_dir, os_name))
 
             # test package
-            print "============================"
-            print "test %s package ..." % os_name
-            print "============================"
+            print("============================")
+            print("test %s package ..." % os_name)
+            print("============================")
             command_test_package_ubuntu(os_name)
+            container_name = 'ossfs_%s'%random_string(5)
+            docker_run(container_name, test_image, volumes, '/bin/bash %s/command/test_package_%s.sh' % (dest_dir, os_name))
+        elif os_name.startswith('anolisos'):
+            # build package
+            print("=============================")
+            print("build %s package ..." % os_name)
+            print("=============================")
+            command_build_package_anolisos(os_name)
+            container_name = 'ossfs_%s'%random_string(5)
+            docker_run(container_name, dev_image, volumes, '/bin/bash %s/command/build_package_%s.sh' % (dest_dir, os_name))
+
+            # test package
+            print("============================")
+            print("test %s package ..." % os_name)
+            print("============================")
+            command_test_package_anolisos(os_name)
             container_name = 'ossfs_%s'%random_string(5)
             docker_run(container_name, test_image, volumes, '/bin/bash %s/command/test_package_%s.sh' % (dest_dir, os_name))
 
